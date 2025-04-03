7d829e917f6576908e0f532c97fbfc88ea4d077c
17luba <146106312+17luba@users.noreply.github.com>
2025-04-03 13:14:12 +0200

    Configuration du système monetaire

--- Assets/Scripts/UserInterface.cs
+++ Assets/Scripts/UserInterface.cs
@@ -7,10 +7,16 @@ public class UserInterface : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI actionText;
     [SerializeField] private GameObject actionGo;
+    [SerializeField] private TextMeshProUGUI moneyText;
 
     private void Awake()
     {
         instence = this;
+    }
+
+    private void Start()
+    {
+        setMoney(7500.32f);
     }
 
     public void ShowAction(string text)
@@ -22,5 +28,10 @@ public class UserInterface : MonoBehaviour
     public void HideAction()
     {
         actionGo.SetActive(false);
+    }
+
+    public void setMoney(float money)
+    {
+        moneyText.text = money.ToString("C");
     }
 }
